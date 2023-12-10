9186d58d120e4491bc7965a74b3830184d50ee54
preeti2703 <152139669+preeti2703@users.noreply.github.com>
2023-12-10 18:04:28 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -12,7 +12,13 @@ def calculate_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Distance matrix
     """
     # Write your logic here
+    pivot_df1 = df1.pivot(index='id_start', columns='id_end', values='distance').fillna(0)
+    
+    distance_matrix = pivot_df1.add(pivot_df1.T, fill_value=0)
 
+    for idx in distance_matrix.index:
+        distance_matrix.loc[idx, idx] = 0
+    
     return df
 
 
@@ -27,6 +33,16 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
+    upper_triangle = distance_matrix.where(
+        np.triu(np.ones(distance_matrix.shape), k=1).astype(bool)
+    )
+        
+    unrolled_df = (
+        upper_triangle.stack().reset_index()
+                      .rename(columns={'id_start': 'id_start', 'id_end': 'id_end', 0: 'distance'})
+    )
+        
+    unrolled_df = unrolled_df.rename_axis(None, axis=1)
 
     return df
 
@@ -44,6 +60,16 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
                           of the reference ID's average distance.
     """
     # Write your logic here
+    avg_distance = result_unrolled[result_unrolled['id_start'] == reference_value]['distance'].mean()
+
+    threshold_lower = avg_distance - (0.1 * avg_distance)
+    threshold_upper = avg_distance + (0.1 * avg_distance)
+
+    within_threshold = distance_df[
+        (result_unrolled['id_start'] != reference_value) &
+        (result_unrolled['distance'] >= threshold_lower) &
+        (result_unrolled['distance'] <= threshold_upper)
+    ]['id_start'].unique()
 
     return df
 
@@ -59,6 +85,16 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
+    rate_coefficients = {
+        'moto': 0.8,
+        'car': 1.2,
+        'rv': 1.5,
+        'bus': 2.2,
+        'truck': 3.6
+    }
+    
+    for vehicle in rate_coefficients:
+        df[vehicle] = df['distance'] * rate_coefficients[vehicle]
 
     return df
 
@@ -74,5 +110,29 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
+     weekday_ranges = [
+        ('00:00:00', '10:00:00', 0.8),
+        ('10:00:00', '18:00:00', 1.2),
+        ('18:00:00', '23:59:59', 0.8)
+    ]
+    weekend_ranges = [('00:00:00', '23:59:59', 0.7)]
+   
+    for index, row in df1.iterrows():
+        start_time = pd.to_datetime(row['start_time']).time()
+        end_time = pd.to_datetime(row['end_time']).time()
+       
+        start_day = pd.to_datetime(row['startDay']).strftime('%A')
+        end_day = pd.to_datetime(row['endDay']).strftime('%A')
+       
+        if start_day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']:
+            for start, end, factor in weekday_ranges:
+                if pd.to_datetime(start_time) >= pd.to_datetime(start).time() and \
+                   pd.to_datetime(end_time) <= pd.to_datetime(end).time():
+                    break
+        else:
+            for start, end, factor in weekend_ranges:
+                if pd.to_datetime(start_time) >= pd.to_datetime(start).time() and \
+                   pd.to_datetime(end_time) <= pd.to_datetime(end).time():
+                    break
 
     return df
